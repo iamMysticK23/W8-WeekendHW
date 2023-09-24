830cd6d35bcff60e131d31733b234b8827424c00
Kenai Epps <k.epps23@gmail.com>
2023-09-24 07:23:34 -0700

    added pics

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+
+// external imports
 import { v4 as uuidv4 } from 'uuid';
 
+
+// Shopping Cart Item attributes
 interface ShoppingCartItem {
     id: string;
     name: string;
     price: number;
     description: string;
+    imageUrl: string;
 }
 
+// Shopping Cart User attributes
 interface ShoppingCartUser {
+
+    // Array to hold a user's cart items 
     cart: ShoppingCartItem[];
     addToCart(item: ShoppingCartItem): void;
     removeFromCart(item: ShoppingCartItem): void;
@@ -17,6 +25,7 @@ interface ShoppingCartUser {
     age: number;
 }
 
+// initiating a new user
 class Person implements ShoppingCartUser {
     private _cart: ShoppingCartItem[] = [];
     private _name: string;
@@ -35,10 +44,14 @@ class Person implements ShoppingCartUser {
         this._cart.push(item);
     }
 
+    // remove all of one specific item from _cart array
     removeFromCart(item: ShoppingCartItem) {
         this._cart = this._cart.filter(cartItem => cartItem.id !== item.id);
     }
 
+
+    // each time "remove one" button is clicked, it removes one from the cart
+    // and reduces the total
     removeQuantityFromCart(item: ShoppingCartItem, quantity: number) {
         for (let i = 0; i < quantity; i++) {
             const index = this._cart.findIndex(cartItem => cartItem.id === item.id);
@@ -54,6 +67,7 @@ class Person implements ShoppingCartUser {
         return this._cart.reduce((total, item) => total + item.price, 0);
     }
 
+    // getters and setters for name and age
     get name(): string {
         return this._name;
     }
@@ -71,28 +85,37 @@ class Person implements ShoppingCartUser {
     }
 }
 
+
+// create new user
 function createUser(name: string, age: number): ShoppingCartUser {
     return new Person(name, age);
 }
 
-function createItem(name: string, price: number, description: string): ShoppingCartItem {
+// create items (6)
+function createItem(name: string, price: number, description: string, imageUrl: string): ShoppingCartItem {
     return {
         id: uuidv4(),
         name,
         price,
         description,
+        imageUrl
     };
 }
 
+// update and display the user's shopping cart
 const updateCartDisplay = (person: ShoppingCartUser | null) => {
+
+    // HTML Elements
     const cartDiv = document.getElementById('cart');
     const totalCostElement = document.getElementById('totalCost');
     const helloUserElement = document.getElementById('helloUser');
 
+    // if all 3 elements exist, then the cart is generated
     if (cartDiv && totalCostElement && person) {
         cartDiv.innerHTML = '';
         const cartItemCounts: { [itemName: string]: { item: ShoppingCartItem, count: number } } = {};
 
+        // iterating through the itens in a user's cart
         person.cart.forEach((item) => {
             const itemName = item.name;
             if (!cartItemCounts[itemName]) {
@@ -105,6 +128,7 @@ const updateCartDisplay = (person: ShoppingCartUser | null) => {
             }
         });
 
+        // display the items in a Bootstrap 5 card
         for (const itemName in cartItemCounts) {
             const { item, count } = cartItemCounts[itemName];
             const itemDiv = document.createElement('div');
@@ -112,25 +136,28 @@ const updateCartDisplay = (person: ShoppingCartUser | null) => {
             const cardBody = document.createElement('div');
             cardBody.classList.add('card-body');
 
+            // button to remove one of a specific item
             const removeOneButton = document.createElement('button');
             removeOneButton.innerText = "Remove One";
-            removeOneButton.classList.add('btn', 'btn-danger', 'me-2');
+            removeOneButton.classList.add('btn', 'btn-warning', 'me-2');
             removeOneButton.addEventListener('click', () => {
                 person.removeQuantityFromCart(item, 1);
                 updateCartDisplay(person);
             });
 
+            //button to remove all of a specific item
             const removeAllButton = document.createElement('button');
             removeAllButton.innerText = "Remove All";
-            removeAllButton.classList.add('btn', 'btn-warning');
+            removeAllButton.classList.add('btn', 'btn-danger');
             removeAllButton.addEventListener('click', () => {
                 person.removeFromCart(item);
                 updateCartDisplay(person);
             });
 
+            // quantity and item description
             cardBody.innerHTML = `
-                <h5 class="card-title">${item.name} - Quantity ${count}</h5>
-                <p class="card-text">$${item.price.toFixed(2)} - ${item.description}</p>
+                <h5 class="card-title">${item.name} - Quantity: ${count}</h5>
+                <p class="card-text">$${item.price.toFixed(2)} </p>
             `;
 
             cardBody.appendChild(removeOneButton);
@@ -138,10 +165,10 @@ const updateCartDisplay = (person: ShoppingCartUser | null) => {
             itemDiv.appendChild(cardBody);
             cartDiv.appendChild(itemDiv);
         }
-
+        // this displays the total cost of all items in acrt
         totalCostElement.textContent = `Total Cost: $${person.cartTotal().toFixed(2)}`;
         if (helloUserElement) {
-            helloUserElement.textContent = `Hello, ${person.name}!`;
+            helloUserElement.innerHTML = `Hello,  ${person.name.charAt(0).toUpperCase() + person.name.slice(1)}<br>Age: ${person.age}!<br><br>Items For Sale:`;
         }
     }
 };
@@ -165,42 +192,74 @@ if (userForm) {
         const newUser = createUser(userName, userAge);
         person = newUser;
 
-        // Update the "Hello user" message and clear the input fields
+        // Updates the "Hello user, age" message and clears the input fields
         updateCartDisplay(newUser);
         userNameInput.value = '';
         userAgeInput.value = '';
 
+        // this hides the shop until a user logs in
         if (shopColumn) {
             shopColumn.style.display = 'block';
         }
     });
 }
 
-// Initialize the shop
+// Shop initialization
 const initShop = () => {
     const shopDiv = document.getElementById('shop');
     const totalCostElement = document.getElementById('totalCost');
     const cartDiv = document.getElementById('cart');
 
+    // if the cart is empty, display $0.00
     if (shopDiv && totalCostElement && cartDiv) {
         shopDiv.innerHTML = '';
         totalCostElement.textContent = 'Total Cost: $0.00';
 
+        // items for sale
         const items: ShoppingCartItem[] = [
-            createItem("Red Apples", 3.00, "Grown in Washington"),
-            createItem("Peaches", 2.50, "Grown in South Georgia"),
-            createItem("Bananas for Donkey Kong", 1.75, "Imported From Mexico"),
-            createItem("Organic Royal Star Papaya", 1.15, "Fresh From Texas"),
-            createItem("Organic Blackberries", 7.50, "Grown in California"),
-            createItem("Strawberries", 5.50, "Grown near Santa Cruz, California"),
+            createItem("Red Apples", 3.00, "Grown in Washington", "https://d3fwccq2bzlel7.cloudfront.net/Pictures/1024x536/4/6/9/42469_2_1201037_e.jpg"),
+            createItem("Peaches", 2.50, "Grown in South Georgia", "https://robbreport.com/wp-content/uploads/2023/06/Georgia-Peaches.jpg"),
+            createItem("Bananas for Donkey Kong", 1.75, "Imported From Mexico", "https://i0.wp.com/mynintendonews.com/wp-content/uploads/2013/05/donkey_kong_bananas.jpg"),
+            createItem("Organic Royal Star Papaya", 1.15, "Fresh From Texas", "https://specialtyproduce.com/sppics/8443.png"),
+            createItem("Organic Blackberries", 7.50, "Grown in California", "https://h2.commercev3.net/cdn.gurneys.com/images/800/74033.jpg"),
+            createItem("Strawberries", 5.50, "Grown near Santa Cruz, California", "https://vegetablegrowersnews.com/wp-content/uploads/2018/07/California-strawberries.jpg"),
         ];
 
+        // Bootstrap 5 cards for each item
         items.forEach((item) => {
             const itemDiv = document.createElement('div');
             itemDiv.classList.add('card', 'mb-3');
+        
             const cardBody = document.createElement('div');
             cardBody.classList.add('card-body');
-
+        
+           
+            const cardRow = document.createElement('div');
+            cardRow.classList.add('row');
+        
+            
+            const imgCol = document.createElement('div');
+            imgCol.classList.add('col-md-6'); 
+        
+            const cardImage = document.createElement('img');
+            cardImage.src = item.imageUrl;
+            cardImage.classList.add('img-fluid', 'rounded'); 
+            cardImage.alt = `Image of ${item.name}`;
+            
+            cardImage.style.width = '100%'; 
+            cardImage.style.minHeight = '150px'; 
+        
+            imgCol.appendChild(cardImage);
+            cardRow.appendChild(imgCol);
+        
+            // Column for Item Details
+            const detailsCol = document.createElement('div');
+            detailsCol.classList.add('col-md-6'); 
+            
+            detailsCol.innerHTML = `
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text">$${item.price.toFixed(2)} - ${item.description}</p>
+            `;
             const addButton = document.createElement('button');
             addButton.innerText = "Add to Cart";
             addButton.classList.add('btn', 'btn-success');
@@ -208,18 +267,17 @@ const initShop = () => {
                 person?.addToCart(item);
                 updateCartDisplay(person);
             });
-
-            cardBody.innerHTML = `
-                <h5 class="card-title">${item.name}</h5>
-                <p class="card-text">$${item.price.toFixed(2)} - ${item.description}</p>
-            `;
-
-            cardBody.appendChild(addButton);
+            detailsCol.appendChild(addButton);
+            cardRow.appendChild(detailsCol);
+        
+            cardBody.appendChild(cardRow);
             itemDiv.appendChild(cardBody);
             shopDiv.appendChild(itemDiv);
         });
+        
+        
     }
 };
 
-// Initialize the shop
+// Call the shop function
 initShop();
